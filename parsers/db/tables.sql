fc6424ecf6301f0351ccb62429994f260775fe98
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-10-14 18:44:21 +0000

    Include ct table

--- parsers/db/tables.sql
+++ parsers/db/tables.sql
@@ -1,9 +1,18 @@
-DROP TABLE IF EXISTS sme;
-CREATE TABLE sme(
-    id TEXT PRIMARY KEY
-);
+DROP TABLE IF EXISTS mri;
+DROP TABLE IF EXISTS ct;
 
 CREATE TABLE mri(
+    id BIGSERIAL PRIMARY KEY,
+    equipment_id VARCHAR(10),
+    host_state VARCHAR(4),
+    host_date DATE,
+    host_time TIME,
+    host_col_1 TEXT,
+    host_col_2 TEXT,
+    host_info TEXT
+);
+
+CREATE TABLE ct(
     id BIGSERIAL PRIMARY KEY,
     equipment_id VARCHAR(10),
     host_state VARCHAR(4),
