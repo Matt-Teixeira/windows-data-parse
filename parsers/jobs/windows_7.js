091e5dab5a7aa9f158f995cd92048ce30e295165
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-10-17 20:38:28 +0000

    Set up to bulk insert into db

--- parsers/jobs/windows_7.js
+++ parsers/jobs/windows_7.js
@@ -2,11 +2,14 @@
 require("dotenv").config({ path: "../../.env" });
 const fs = require("node:fs").promises;
 const { log } = require("../logger");
+const { getSME, testModality } = require("../utils/regExTests");
+const bulkInsert = require("../utils/queryBuilder");
+const convertDates = require("../utils/dates");
 
 const parse_win_7 = async (filePath) => {
-  const smeRe = /SME\d{5}/;
-  const SME = filePath.match(smeRe);
-
+  const data = [];
+  const SME = getSME(filePath);
+  try {
   await log("info", "NA", "NA", "parse_win_7", "FN CALL", {
     sme: SME,
     file: filePath,
@@ -18,16 +21,42 @@ const parse_win_7 = async (filePath) => {
   const smallGroupRe =
     /Source:(?<source_group>.*)[\r\n]Domain:(?<domain_group>.*)[\r\n]Type:(?<type_group>.*)[\r\n]ID:(?<id_group>.*)[\r\n](Date:.*\s(?<month>\w+)\s(?<day>\d+),\s(?<year>\d+),\s(?<time>.*))[\r\n]Text:(?<text_group>.*)\n?/;
 
-  try {
-    const data = (await fs.readFile(filePath)).toString();
+  let count = 1;
 
-    let matches = data.matchAll(bigGroupRe);
+  
+    const fileData = (await fs.readFile(filePath)).toString();
+
+    let matches = fileData.matchAll(bigGroupRe);
     let matchesArray = [...matches];
 
+    let modality;
     for await (let match of matchesArray) {
+      let row = [];
+
       let matchGroups = match.groups.big_group.match(smallGroupRe);
 
-      await log("info", "NA", `${SME}`, "readFile", "FN CALL", {
+      convertDates(matchGroups.groups);
+
+      // Get modality from first line
+      if (count === 1) {
+        modality = (
+          await testModality(matchGroups.groups.source_group)
+        ).toLowerCase();
+      }
+
+      row.push(
+        SME[0],
+        matchGroups.groups.source_group,
+        matchGroups.groups.host_date,
+        matchGroups.groups.time,
+        matchGroups.groups.domain_group,
+        matchGroups.groups.type_group,
+        matchGroups.groups.id_group,
+        matchGroups.groups.text_group
+      );
+      data.push(row);
+
+      /*  await log("info", "NA", `${SME}`, "readFile", "FN CALL", {
         source_group: matchGroups.groups.source_group,
         domain_group: matchGroups.groups.domain_group,
         type_group: matchGroups.groups.type_group,
@@ -36,14 +65,30 @@ const parse_win_7 = async (filePath) => {
         day: matchGroups.groups.day,
         year: matchGroups.groups.year,
         text_group: matchGroups.groups.text_group,
-      });
+      }); */
+
+      count++;
     }
+    await bulkInsert(
+      data,
+      modality,
+      [
+        "equipment_id",
+        "source_group",
+        "host_date",
+        "host_time",
+        "domain_group",
+        "type_group",
+        "id_group",
+        "text_group",
+      ],
+      filePath
+    );
   } catch (error) {
     await log("error", "NA", `${SME}`, "parse_win_7", "FN CATCH", {
       error: error,
     });
   }
 };
-
 
 module.exports = parse_win_7;
