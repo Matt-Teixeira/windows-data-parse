c26aa4c5ff3839575deabc2bdf5a0b71fcc2ef53
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-10-17 20:32:40 +0000

    Updated tables to accommodate windows 7 data

--- parsers/db/tables.sql
+++ parsers/db/tables.sql
@@ -7,6 +7,34 @@ CREATE TABLE mri(
     host_state VARCHAR(4),
     host_date DATE,
     host_time TIME,
+    source_group TEXT,
+    type_group INT,
+    text_group TEXT,
+    domain_group TEXT,
+    id_group INT
+);
+
+CREATE TABLE ct(
+    id BIGSERIAL PRIMARY KEY,
+    equipment_id VARCHAR(10),
+    host_state VARCHAR(4),
+    host_date DATE,
+    host_time TIME,
+    source_group TEXT,
+    type_group INT,
+    text_group TEXT,
+    domain_group TEXT,
+    id_group INT
+);
+
+
+
+CREATE TABLE mri(
+    id BIGSERIAL PRIMARY KEY,
+    equipment_id VARCHAR(10),
+    host_state VARCHAR(4),
+    host_date DATE,
+    host_time TIME,
     host_col_1 TEXT,
     host_col_2 TEXT,
     host_info TEXT
