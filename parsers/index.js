17e91c8eaf7d006c3386c467260b07ae814f6802
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-10-18 14:07:58 +0000

    Condition to not run win 7 if win 10 successful

--- parsers/index.js
+++ parsers/index.js
@@ -6,17 +6,19 @@ const parse_win_10 = require("./jobs/windows_10");
 // CT: SME00811 SME00812(syntax error at or near "(") SME00816
 // MRI: SME01107 SME01109 SME01112
 // /opt/hhm-files/C0137/SHIP009/SME00812/EvtApplication_Today.txt
-//const path = "/opt/hhm-files/C0137/SHIP009/SME01107/EvtApplication_Today.txt";
-const path = "SME00001_win_7.txt";
+const path = "/opt/hhm-files/C0137/SHIP009/SME01107/EvtApplication_Today.txt";
+//const path = "SME00001_win_7.txt";
 
 const runJob = async (filePath) => {
   await log("info", "NA", "NA", "runJob", "FN CALL", {
     // sme: SME,
+    
     file: filePath,
   });
 
   try {
     const parsed_data = await parse_win_10(filePath);
+    console.log(parsed_data)
     if (!parsed_data) {
       await parse_win_7(filePath);
     }
